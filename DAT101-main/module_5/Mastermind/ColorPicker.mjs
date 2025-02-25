a90fa0589051c9f981dabdfb0c1963c206fbb387
stine Lyse <stinely@uia.no>
2025-02-25 17:16:42 +0100

    oppdatering

---
+++ DAT101-main/module_5/Mastermind/ColorPicker.mjs
@@ -0,0 +1,39 @@
+"use strict";
+import lib2D from "../../common/libs/lib2d_v2.mjs";
+import libSprite from "../../common/libs/libSprite_v2.mjs";
+import MastermindBoard from "./MastermindBoard.mjs";
+import { GameProps } from "./Mastermind.mjs";
+
+const Positions = MastermindBoard.ColorPicker;
+
+export class TColorPicker extends libSprite.TSpriteDraggable {
+  #spcvs;
+  #spi;
+  #color;
+  constructor(spcvs, spriteInfo, color, index){ 
+    super(spcvs, spriteInfo,Positions[color]); //super er en referanse til superklassen
+    this.index = index; 
+    this.snapTo = GameProps.snapTo;
+    this.#spcvs = spcvs;
+    this.#spi = spriteInfo;
+    this.#color = color;
+  }
+
+  onCanDrop(){ 
+    return false;
+  }
+
+  clone(){ 
+    return new TColorPicker(
+      this.#spcvs,
+      this.#spi,
+      this.#color,
+      this.index
+    )
+  }
+
+  onDrop(){
+    GameProps.ColorPickers.push(this.clone());
+  }
+
+} 
--- DAT101-main/module_5/Mastermind/Mastermind.mjs
+++ DAT101-main/module_5/Mastermind/Mastermind.mjs
@@ -5,6 +5,8 @@
 //--------------------------------------------------------------------------------------------------------------------
 import lib2D from "../../common/libs/lib2d_v2.mjs";
 import libSprite from "../../common/libs/libSprite_v2.mjs";
+import { TColorPicker } from "./ColorPicker.mjs";
+import MastermindBoard from "./MastermindBoard.mjs";
 
 //--------------------------------------------------------------------------------------------------------------------
 //------ Variables, Constants and Objects
@@ -26,7 +28,17 @@ const spcvs = new libSprite.TSpriteCanvas(cvs);
 
 //Add all you game objects here
 export const GameProps = {
- 
+ Board: null, //tegner brettet
+ ButtonNewGame: null,
+ ButtonCheckAnswer: null,
+ ButtonCheat: null,
+ PanelHideAnswer: null,
+ ColorPickers: null,
+ snapTo: {
+  positions: MastermindBoard.ColorAnswer.Row1,
+  distance: 20
+ },
+ ColorHint: null,
 }
 
 //--------------------------------------------------------------------------------------------------------------------
@@ -34,11 +46,24 @@ export const GameProps = {
 //--------------------------------------------------------------------------------------------------------------------
 
 function newGame() {
+  
 }
 
 function drawGame(){
   spcvs.clearCanvas();
   //Draw all game objects here, remember to think about the draw order (layers in PhotoShop for example!)
+  GameProps.Board.draw(); //tegner brettet
+  GameProps.ButtonNewGame.draw();
+  GameProps.ButtonCheckAnswer.draw();
+  GameProps.ButtonCheat.draw();
+  GameProps.PanelHideAnswer.draw();
+  GameProps.ColorHint.draw();
+
+  //tegner fargene på colorpicker, bruker for-løkke for å tegne alle fargene fordi det er flere farger og det er lettere å bruke en for-løkke istedenfor å skrive hver enkelt farge.
+  for (let i = 0; i < GameProps.ColorPickers.length; i++) { 
+    const ColorPicker = GameProps.ColorPickers[i];
+    ColorPicker.draw();
+  }
   
   requestAnimationFrame(drawGame);
 }
@@ -52,7 +77,25 @@ function loadGame() {
   //Set canvas with and height to match the sprite sheet
   cvs.width = SpriteInfoList.Board.width;
   cvs.height = SpriteInfoList.Board.height;
+  spcvs.updateBoundsRect();
+  const pos = new lib2D.TPoint(0, 0);
+  GameProps.Board = new libSprite.TSprite(spcvs, SpriteInfoList.Board, new lib2D.TPoint(0, 0)); //tegner brettet //Tspritebutton endrer musen til en hånd som peker
+  GameProps.ButtonNewGame = new libSprite.TSpriteButton(spcvs, SpriteInfoList.ButtonNewGame, new lib2D.TPoint(275, 5));
+  GameProps.ButtonCheckAnswer = new libSprite.TSpriteButton(spcvs, SpriteInfoList.ButtonCheckAnswer, new lib2D.TPoint(275, 53));
+  GameProps.ButtonCheat = new libSprite.TSpriteButton(spcvs, SpriteInfoList.ButtonCheat, new lib2D.TPoint(5, 45));
+  GameProps.PanelHideAnswer = new libSprite.TSpriteButton(spcvs, SpriteInfoList.PanelHideAnswer, new lib2D.TPoint(84, 45));
+  GameProps.ColorHint = new libSprite.TSprite(spcvs, SpriteInfoList.ColorHint, new lib2D.TPoint(0, 0));
 
+GameProps.ColorPickers = [ //importerer alle fargene på colorpicker
+  new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Black", 0),
+  new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Blue", 1),
+  new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Brown", 2),
+  new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Green", 3),
+  new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Orange", 4),
+  new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Red", 5),
+  new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "White", 6), 
+  new TColorPicker(spcvs, SpriteInfoList.ColorPicker, "Yellow", 7),
+ ];
   newGame();
   requestAnimationFrame(drawGame); // Start the animation loop
 }
@@ -64,3 +107,4 @@ function loadGame() {
 
 
 spcvs.loadSpriteSheet("./Media/SpriteSheet.png", loadGame);
+window.addEventListener("resize", () => spcvs.updateBoundsRect());
